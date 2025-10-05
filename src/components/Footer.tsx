6436916607fcf3924489da782160611ff0cc9d9e
Abdur Rahman <ar7543078@gmail.com>
2025-10-05 10:48:27 +0600

    UI:Footer design completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg text-white">
+      <div className="flex flex-col items-center md:items-start gap-4">
+        <Link href={"/"} className="flex items-center">
+          <Image
+            src={"/logo.png"}
+            alt="E-Com"
+            width={36}
+            height={36}
+            className="h-6 w-6 md:w-9 md:h-9"
+          />
+          <p className="text-xl font-medium tracking-wider hidden md:block">
+            E-Com
+          </p>
+        </Link>
+        <p className="text-sm text-gray-400">© 2025 E-Com.</p>
+        <p className="text-sm text-gray-400"> All rights reserved.</p>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/"}>Term Of Services</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/"}>Term Of Services</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <Link href={"/"}>Term Of Services</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
