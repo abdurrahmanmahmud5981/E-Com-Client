a746ec605e68eea862850f3ffaf545e3274f0f36
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-10-26 20:29:56 +0600

    UI: Implement Shipping and Payment forms with validation and state management

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export type ProductType = {
     id: string | number;
     name: string;
@@ -19,3 +21,25 @@ export type CartItemType = ProductType & {
 }
 
 export type CartItemsType = CartItemType[]
+
+
+export const shippinFormSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email().min(1,"Email is required!"),
+    phone: z.string().min(7, "Phone must be between 7 and 10 digits!").max(10,"Phone must be between 7 and 10 digits!").regex(/^\d+$/,"Phone number only contain numbers!"),
+    address:z.string().min(1,"Address is required!"),
+    city:z.string().min(1,"City is required!"),
+}) 
+
+export type ShippingFormInpust = z.infer<typeof shippinFormSchema> ;
+
+
+export const paymentFormSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email().min(1,"Email is required!"),
+    phone: z.string().min(7, "Phone must be between 7 and 10 digits!").max(10,"Phone must be between 7 and 10 digits!").regex(/^\d+$/,"Phone number only contain numbers!"),
+    address:z.string().min(1,"Address is required!"),
+    city:z.string().min(1,"City is required!"),
+}) 
+
+export type PaymentFormInpust = z.infer<typeof paymentFormSchema> ;
